af8d08ecb0723c9ccc3521300ac943437c71c7ab
BangyStudios <bangystudios@gmail.com>
2023-06-27 17:04:28 +1000

    Added save to CSV database and backoff strategy.

--- main.py
+++ main.py
@@ -3,24 +3,42 @@ import query
 import database
 import time
 from requests.exceptions import ConnectionError
-from pyppeteer.errors import TimeoutError, PageError
+from pyppeteer.errors import TimeoutError, PageError, BrowserError
 
 config:dict = config.ConfigReader().read() # Read config
 
 crawler = query.Crawler(config)
-calculator = query.Calculator()
 csv = database.CSV(config)
 txt = database.TempFile(config)
-# csv.write_titles_csv(crawler.data["titles_reduced"])
+
+csv.write_titles_csv(crawler.data["titles_reduced"])
+
+# backoff strategy
+backoff_time = 1
+
 while True:
     try:
         crawler.update_data()
-        # csv.write_values_csv(crawler.data["values_reduced"])
-        net_temp = calculator.get_net(crawler.data)
-        txt.write_net_temp([net_temp])
-        print(f"Net: {net_temp}")
-        time.sleep(25)
-    except (ConnectionError, TimeoutError, PageError):
-        print("Connection failed, retrying in 10s...")
-        time.sleep(10)
+        title_value_pairs = zip(crawler.data["titles_reduced"], crawler.data["values_reduced"])
+        production = next((v for t, v in title_value_pairs if t == "Production"), 0)
+        consumption = next((v for t, v in title_value_pairs if t == "Consumption"), 0)
+        
+        csv.write_values_csv(crawler.data["values_reduced"])
+        
+        net = production - consumption
+        txt.write_net_temp([net])
+        print(f"Production: {production}, Consumption: {consumption}, Net: {net}")
+
+        # Reset backoff time upon successful connection
+        backoff_time = 1
+
+        # Sleep until the next minute
+        time_to_next_minute = 60 - time.time() % 60
+        time.sleep(time_to_next_minute)
+
+    except (ConnectionError, TimeoutError, PageError, BrowserError) as e:
+        print(f"Connection failed due to {str(e)}, retrying in {backoff_time}s...")
+        time.sleep(backoff_time)
+        # Increase backoff time for the next potential failure
+        backoff_time *= 2
         continue
